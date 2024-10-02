16d59e59c628d37e890be99112cada5e5139d8d1
Felipe Torquato <112289573+FELPSTS@users.noreply.github.com>
2024-10-02 10:47:20 -0300

    Add files via upload

--- funcionario-/models.py
+++ funcionario-/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class FUNCIONARIO(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=255)
+    empresa_pertence = models.CharField(max_length=255)
+    id_empresa_pertence = models.CharField(max_length=255)
+    adm_responsavel = models.CharField(max_length=255)
+    id_adm_responsavel = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username
